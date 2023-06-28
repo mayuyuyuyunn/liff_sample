2e91c9635446e76270f94ca44e546edc9ec2e03f
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-06-28 17:29:15 +0900

    Add: static_pagesのコントローラーでログイン前と後でページを分ける

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def top
+    if current_user.nil?
+      render :before_login
+    else
+      render :after_login
+    end
   end
 
-  def before_login
-  end
+  def before_login; end
 
-  def after_login
-  end
+  def after_login; end
 end
