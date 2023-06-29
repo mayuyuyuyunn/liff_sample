b22de574bcf09f12d852571eef64fec3756394c7
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2023-06-29 12:11:12 +0900

    Add: トップページのルーティング変えた

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-  get 'static_pages/before_login'
-  get 'static_pages/after_login'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#top'
 end
